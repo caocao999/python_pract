638350921d84ac9780f769d3d59d1d12cd17d851
caocao999 <maigret@ya2.so-net.ne.jp>
2020-08-28 20:12:06 +0900

    アプリの完成度を高めた

--- app002.py
+++ app002.py
@@ -8,37 +8,8 @@ from datetime import date
 import time
 
 
-#勤務場所を記入　
-def workingplace():
-    tmp = input("勤務場所を入力してださい")
-    s_place = "　[勤務地]:"+tmp
-    return s_place
-
-#勤務時間を記入　
-def workingtime():
-    w_begin = input("勤務開始時間を入力してださい")
-    w_end = input("勤務終了時間を入力してださい")
-    s_time = "　[勤務時間]:{0} ~ {1}".format(w_begin,w_end)
-    return s_time
-
-#勤務内容
-def workingtasks():
-    task = input("行った作業内容を入力してください。")
-    task = " [作業内容]:"+task
-    return task
-
-print(workingplace())
-
-
-
-
-
-
-
-
-print(sys.argv[0])
-
-if  len(sys.argv) != 3:
+#===========================================
+if  len(sys.argv) != 2:
     print("お名前（途中に空白なし）を入力してください")
     sys.exit(1)
 
@@ -49,3 +20,53 @@ header = """
 {1}の業務報告をいたします。
 
 """.format(sys.argv[1],date.today())
+
+
+
+#勤務場所を記入　
+def workingplace():
+    tmp = input("勤務場所を入力してださい")
+    s_place = "[勤務地]:"+tmp
+    return s_place
+
+#勤務時間を記入　
+def workingtime():
+    w_begin = input("勤務開始時間を入力してださい")
+    w_end = input("勤務終了時間を入力してださい")
+    s_time = "[勤務時間]:{0} ~ {1}".format(w_begin,w_end)
+    return s_time
+
+#勤務内容
+def workingtasks():
+    task = input("行った作業内容を入力してください。")
+    task = "[作業内容]:"+task
+    return task
+
+
+
+
+
+#==============================================================
+#
+#  ファイルへの書き込み
+#
+title = "業務報告{0}.txt".format(date.today())
+
+m_place = workingplace()
+m_time = workingtime()
+m_tasks = workingtasks()
+
+
+
+body = """
+{0}
+{1}
+{2}
+""".format(m_place,m_time,m_tasks)
+
+
+
+file = open(title,"w")
+file.write(header)
+file.write(body)
+file.close()
